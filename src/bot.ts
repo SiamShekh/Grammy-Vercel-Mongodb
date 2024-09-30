0a50d15b05d2451b2eef8352e6956315dcbfe83e
Md. Siam Shekh <siam62349@gmail.com>
2024-09-30 12:47:54 +0600

    crud

--- src/bot.ts
+++ src/bot.ts
@@ -1,32 +1,78 @@
-import { Bot, InlineKeyboard } from 'grammy'
-import { MongoClient } from 'mongodb'
+import { Bot, Context, InlineKeyboard } from 'grammy'
 import 'dotenv/config'
+import { MongoClient } from 'mongodb';
 
 const {
     BOT_TOKEN: token, MONGODB_URI
 } = process.env
 
 export const bot = new Bot(token as string);
+const url = MONGODB_URI;
+const client = new MongoClient(url as string);
 
 bot.command('start', async ctx => {
-    await ctx.reply('Welcome to ragdoll')
+    await ctx.reply('Welcome to Cricbuzz')
+});
+
+bot.command("me", async (ctx: Context) => {
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db("grammy_magic");
+    const collection = db.collection('user');
+    const user = await collection.findOne({ id: ctx?.from?.id as number });
+    if (user?._id) {
+        return ctx.reply(`Name: ${user?.first_name} \nUsername: @${user?.username}\nID: ${user?.id}`)
+    } else {
+        return ctx.reply("The user is not exist on server!")
+    }
 })
 
-bot.command("refer", async (ctx: any) => {
+bot.command("update", async (ctx: Context) => {
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db("grammy_magic");
+    const collection = db.collection('user');
+    const user = await collection.findOneAndUpdate({ id: ctx?.from?.id as number }, {
+        $set: {
+            first_name: ctx?.message?.text?.split("/update")[1].trim() as string
+        },
+    },
+        { returnDocument: "after" }
+    );
 
-    const url = MONGODB_URI as string;
-    const client = new MongoClient(url);
-    client.connect().then(() => console.log("Server is connected!"));
-    const db = client.db("ragdoll");
-    const usersCollection = db.collection("users");
-
-    const keyboard = new InlineKeyboard()
-        .url('Open App', 'https://t.me/Ragdoll_App_Bot/app');
-
-    const user = await usersCollection.findOne({ TgId: String(ctx?.from?.id) });
-    if (!user?.ReferCode) {
-        return ctx.reply(`<b>Hi ${ctx.from.first_name}!</b> \n\n It seems like you haven't opened our mini app yet. \n Could you please open the app, complete your registration, and get your referral link?`, { reply_markup: keyboard, parse_mode: "HTML" })
-    };
-
-    return ctx.reply(`<b>Hi ${ctx.from.first_name}!</b> \n\n <b>Invitintion link:</b> https://t.me/Ragdoll_App_Bot/app?startparam=${user?.ReferCode}  \n\n<u>Refer list:</u> \n 1. Siam Sheikh \n 2. Naim Kha`, { reply_markup: keyboard, parse_mode: "HTML" })
+    if (user?._id) {
+        return ctx.reply(`Name: ${user?.first_name} \nUsername: @${user?.username}\nID: ${user?.id}`)
+    } else {
+        return ctx.reply("The user is not exist on server!")
+    }
 })
+
+bot.command("delete", async (ctx: Context) => {
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db("grammy_magic");
+    const collection = db.collection('user');
+    const user = await collection.findOneAndDelete({ id: ctx?.from?.id as number });
+    if (user?._id) {
+        return ctx.reply(`User is deleted!`)
+    } else {
+        return ctx.reply("The user is not exist on server!")
+    }
+})
+
+bot.on("message", async (ctx: Context) => {
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db("grammy_magic");
+    const collection = db.collection('user');
+    const user = await collection.findOne({ id: ctx?.from?.id as number });
+
+    if (!user?._id) {
+        await collection.insertOne(ctx?.from as {});
+        console.log("User is created!");
+    } else {
+        console.log("User already exits!");
+    }
+})
+
+bot.start();
